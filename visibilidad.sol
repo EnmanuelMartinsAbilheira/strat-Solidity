eb4b8f3d8203b9ce7e2dd07aba25684b915d26a8
Enmanuel Martins <eel2000martins@hotmail.com>
2022-02-24 15:35:34 +0000

    start visibilidad

---
+++ visibilidad.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.8.1;
+
+contract Visibilidad {
+
+    function x() private view returns(uint){
+        uint data = 9;
+        return data;
+    }
+
+    function y() public view returns(uint){
+        return x();
+    }
+
+}
