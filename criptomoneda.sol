bc441d1e25e92f3251cec1789a37a9ec25267f10
Enmanuel Martins <eel2000martins@hotmail.com>
2022-02-27 18:30:20 +0000

    start configuracion cripto moneda

---
+++ criptomoneda.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.8.1;
+
+contract Coin {
+
+    address public minter;
+    mapping (address => uint) public balances;
+
+    event Sent(address from, address to, uint amount);
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+}
